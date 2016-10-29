183be450b6fd2e4886e656ba42b2f9dbc807a900
DivSyntax <DivSyntax@DESKTOP-4FO1AUD>
2016-10-28 17:37:19 -0700

    New changes

--- FarmerGUI/FarmerGUI/Form1.Designer.cs
+++ FarmerGUI/FarmerGUI/Form1.Designer.cs
@@ -38,10 +38,26 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.malcomBroadnaxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameMalcomBroadnaxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructorBobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assignment8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.date1012016ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.v10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.farmer1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chicken2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grain)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // farmer1
@@ -135,6 +151,129 @@
             this.checkBox3.Text = "checkBox3";
             this.checkBox3.UseVisualStyleBackColor = true;
             // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 183);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 351);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 10;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(12, 540);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 11;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 586);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 66);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(891, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.malcomBroadnaxToolStripMenuItem,
+            this.versionToolStripMenuItem,
+            this.instructionsToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(40, 20);
+            this.toolStripMenuItem1.Text = "Info";
+            // 
+            // malcomBroadnaxToolStripMenuItem
+            // 
+            this.malcomBroadnaxToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nameMalcomBroadnaxToolStripMenuItem,
+            this.instructorBobToolStripMenuItem,
+            this.assignment8ToolStripMenuItem,
+            this.date1012016ToolStripMenuItem});
+            this.malcomBroadnaxToolStripMenuItem.Name = "malcomBroadnaxToolStripMenuItem";
+            this.malcomBroadnaxToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.malcomBroadnaxToolStripMenuItem.Text = "Class";
+            // 
+            // nameMalcomBroadnaxToolStripMenuItem
+            // 
+            this.nameMalcomBroadnaxToolStripMenuItem.Name = "nameMalcomBroadnaxToolStripMenuItem";
+            this.nameMalcomBroadnaxToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.nameMalcomBroadnaxToolStripMenuItem.Text = "Name: Malcom Broadnax";
+            // 
+            // instructorBobToolStripMenuItem
+            // 
+            this.instructorBobToolStripMenuItem.Name = "instructorBobToolStripMenuItem";
+            this.instructorBobToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.instructorBobToolStripMenuItem.Text = "Instructor: Bob";
+            // 
+            // assignment8ToolStripMenuItem
+            // 
+            this.assignment8ToolStripMenuItem.Name = "assignment8ToolStripMenuItem";
+            this.assignment8ToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.assignment8ToolStripMenuItem.Text = "Assignment: 8";
+            // 
+            // date1012016ToolStripMenuItem
+            // 
+            this.date1012016ToolStripMenuItem.Name = "date1012016ToolStripMenuItem";
+            this.date1012016ToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.date1012016ToolStripMenuItem.Text = "Date: 10/1/2016";
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.v10ToolStripMenuItem});
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.versionToolStripMenuItem.Text = "Version";
+            // 
+            // v10ToolStripMenuItem
+            // 
+            this.v10ToolStripMenuItem.Name = "v10ToolStripMenuItem";
+            this.v10ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.v10ToolStripMenuItem.Text = "V1.0";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.Text = "Choose one check box at a time.";
+            // 
             // FarmerGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -142,6 +281,10 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(891, 673);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
@@ -151,7 +294,9 @@
             this.Controls.Add(this.fox);
             this.Controls.Add(this.chicken2);
             this.Controls.Add(this.farmer1);
+            this.Controls.Add(this.menuStrip1);
             this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FarmerGUI";
             this.Text = "FarmerGUI";
             this.Load += new System.EventHandler(this.FarmerGUI_Load);
@@ -159,6 +304,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.chicken2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.grain)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,6 +322,21 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem malcomBroadnaxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nameMalcomBroadnaxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructorBobToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assignment8ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem date1012016ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem v10ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
     }
 }
 
--- FarmerGUI/FarmerGUI/Form1.cs
+++ FarmerGUI/FarmerGUI/Form1.cs
@@ -14,8 +14,7 @@ namespace FarmerGUI
     public partial class FarmerGUI : Form
     {
 
-        List<int> North = new List<int>();
-        List<int> South = new List<int>();
+      
     
 
         public FarmerGUI()
@@ -32,6 +31,7 @@ namespace FarmerGUI
         {
             Farmer farmer = new Farmer();
             var rbuttons = new List<CheckBox> {checkBox1,checkBox2,checkBox3 };
+            var lbuttons = new List<CheckBox> { checkBox4, checkBox5, checkBox6 };
             int x = farmer1.Location.X;
             int y = fox.Location.X;
             int z = grain.Location.X;
@@ -41,13 +41,16 @@ namespace FarmerGUI
 
             farmer1.Location = new Point(x - 50);
 
+         
+
+      
 
 
             if (x <= 0 && checkBox2.Checked)
             {
                 //farmer1.Visible = false;
-
-                checkBox1.Checked = false;
+                chicken2.Location = new Point(36,86);
+                checkBox1.Checked = false; 
                 checkBox3.Checked = false;
                 chicken2.Left = -50;
                 farmer1.Left = 0;
@@ -56,9 +59,10 @@ namespace FarmerGUI
                 //this.InitializeComponent();
                 MessageBox.Show("go bck for rest.");
 
-            }
+            }else
             if(x <= 0 && checkBox1.Checked)
             {
+                grain.Location = new Point(14,451);
                 checkBox2.Checked = false;
                 checkBox3.Checked = false;
                 grain.Left = -50;
@@ -67,10 +71,11 @@ namespace FarmerGUI
                 farmer1.Location = new Point(252, 456);
                 //this.InitializeComponent();
                 MessageBox.Show("go bck for rest.");
-            }
+            }else
 
             if (x <= 0 && checkBox3.Checked)
             {
+                fox.Location = new Point(36,420);
                 checkBox1.Checked = false;
                 checkBox2.Checked = false;
                 fox.Left = -50;
@@ -79,12 +84,14 @@ namespace FarmerGUI
                 farmer1.Location = new Point(252, 456);
                 //this.InitializeComponent();
                 MessageBox.Show("go bck for rest.");
-            }
+            }else
 
             if(chicken2.Left == 0 && grain.Left == 0 && fox.Left == 0)
             {
                 MessageBox.Show("YOU WIN!!");
             }
+
+         
 
             //this.Controls.Clear();
 
@@ -97,5 +104,51 @@ namespace FarmerGUI
         {
             Application.Exit();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       
+            int x = farmer1.Location.X;
+            int y = fox.Location.X;
+            int z = grain.Location.X;
+            int a = chicken2.Location.X;
+
+
+            farmer1.Location = new Point(x + 50);
+            //chicken2.Location = new Point(a + 40);
+            if (x >= 600 && checkBox4.Checked)
+            {
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                chicken2.Location = new Point(672,276);
+                //chicken2.Left = + 50;
+                farmer1.Left = 0;
+               // chicken2.Left = 0;
+                farmer1.Location = new Point(252, 456);
+                MessageBox.Show("Home!");
+            }else if(x >= 600 && checkBox5.Checked)
+            {
+                checkBox4.Checked = false;
+                checkBox6.Checked = false;
+                grain.Location = new Point(617, 43);
+                //chicken2.Left = + 50;
+                farmer1.Left = 0;
+                // chicken2.Left = 0;
+                farmer1.Location = new Point(252, 456);
+                MessageBox.Show("Home!");
+            }else if(x >= 600 && checkBox6.Checked)
+            {
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                fox.Location = new Point(589,423);
+                //chicken2.Left = + 50;
+                farmer1.Left = 0;
+                // chicken2.Left = 0;
+                farmer1.Location = new Point(252, 456);
+                MessageBox.Show("Home!");
+            }
+        }
+
+     
     }
 }
